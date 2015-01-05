4947536966164ca482263ca9be1e82833a4a05b0
Mesut Güneş <gunesmes@gmail.com>
2015-01-06 00:09:19 +0200

    command line parsin is added

--- README.md
+++ README.md
@@ -3,24 +3,30 @@ subtitle_converter
 
 This can be used for converting subtitles. There are two main functionality which you can set the time differences by adding or subtracting seconds with the function set_time_dif() and you can change the frequency (FPS) of the subtitles with convert_substitle().
 
-    subFile = os.path.dirname(os.path.abspath(srt[i])) +"\\" + srt[i]
-    
-    """
-    def convert_substitle(self, fileName, fps_org, fps_tar):
-    this function converts a subtitle file from original frequency to desired 
-    frequency, the frequency of video
-    fileName: the name of the subtitle file
-    fps_org: the frequency of the subtitle
-    fps_tar: the frequency you want to have  
-    """     
-    s.convert_substitle(subFile, 25, 23.976)
-    
-    
-    """
-    def set_time_dif(self, fileName, add_sub, time):
-    this function adds or subtract from the time of subtitle to set-up for videos.  
-    fileName: the name of the subtitle file
-    add_sub: option: type "add" for adding time or "sub" for subtract
-    time: time in second the set-up difference
-    """
-    s.set_time_dif(subFile, 'add', 2.2)
+Make sure you have Python 2.7 or above installed
+Check Python:
+```shell
+python --version
+```
+
+Clone the project:
+```shell
+git clone https://github.com/gunesmes/subtitle_converter.git
+```
+   
+# To run the translater
+* you must have a valid subtitle in .srt format
+
+* run python file
+```shell
+python run.py <function> <path/to/files> <arg3> <arg4> 
+
+# convert the fps from 25 to 23.976
+python run.py convert '/Users/mesutgunes/Projects/subtitle_converter' 25 23.976
+
+# add 2.5 seconds
+python run.py set '/Users/mesutgunes/Projects/subtitle_converter' add 2.5
+
+# subtract 2.5 seconds
+python run.py set '/Users/mesutgunes/Projects/subtitle_converter' sub 2.5
+````
--- run.py
+++ run.py
@@ -4,42 +4,76 @@
 # Author Github username: gunesmes
 
 from subConverter import SubsConverter
-import os
+import os, sys, getopt
 
-#set file_dir = "D:/..." the path of files to be converted
-file_dir = "D:/workspace/subtitle_converter/files/"
-path = os.listdir(file_dir)
 
-#set directory
-os.chdir(file_dir)
+def converter(argv):
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], "options")
+    except getopt.GetoptError, err:
+      print err
+      sys.exit(2)
 
-s = SubsConverter()
+    try:
+        # function that will be used
+        function = args[0]
 
-srt = list()
-for item in path:
-    if item.rfind(".srt") != -1:
-        srt.append(item)
-        continue
+        #set file_dir = "D:/..." the path of files to be converted
+        #file_dir = "/Users/mesutgunes/Projects/subtitle_translator"
+        file_dir = args[1]
 
-for i in range(len(srt)):
-    subFile = os.path.dirname(os.path.abspath(srt[i])) +"\\" + srt[i]
-    
-    """
-    def convert_substitle(self, fileName, fps_org, fps_tar):
-    this function converts a subtitle file from original frequency to desired 
-    frequency, the frequency of video
-    fileName: the name of the subtitle file
-    fps_org: the frequency of the subtitle
-    fps_tar: the frequency you want to have  
-    """     
-    s.convert_substitle(subFile, 25, 23.976)
-    
-    
-    """
-    def set_time_dif(self, fileName, add_sub, time):
-    this function adds or subtract from the time of subtitle to set-up for videos.  
-    fileName: the name of the subtitle file
-    add_sub: option: type "add" for adding time or "sub" for subtract
-    time: time in second the set-up difference
-    """
-    s.set_time_dif(subFile, 'add', 2.2)
+        # set third and forth parameters
+        arg3 = args[2]
+        arg4 = args[3]
+    except IndexError:
+        print "Arguments Error! Please run the file with the following format:" 
+        print "\n   python run.py 'path/to/files' 'max-length-of-lines' 'translator:google or yandex' 'source language' 'target language'\n   python run.py /Users/mesutgunes/Projects/subtitle_translator 40 google pl tr\n"
+        sys.exit(2)
+
+    #set path
+    path = os.listdir(file_dir)
+
+    #set directory
+    os.chdir(file_dir)
+
+    s = SubsConverter()
+
+    srt = list()
+    if len(srt) == 0:
+        print "There is no .srt files. Check the directory!"
+        
+    for item in path:
+        if item.rfind(".srt") != -1:
+            srt.append(item)
+            continue
+
+    for i in range(len(srt)):
+        # windows
+        #subFile = os.path.dirname(os.path.abspath(srt[i])) +"\\" + srt[i]
+        
+        # unix based
+        subFile = os.path.dirname(os.path.abspath(srt[i])) +"/" + srt[i]
+        
+
+        if function == "convert":
+            # def convert_substitle(self, fileName, fps_org, fps_tar):
+            # this function converts a subtitle file from original frequency to desired 
+            # frequency, the frequency of video
+            # fileName: the name of the subtitle file
+            # fps_org: the frequency of the subtitle
+            # fps_tar: the frequency you want to have  
+            s.convert_substitle(subFile, float(arg3), float(arg4))
+
+        elif function == "set":
+            # def set_time_dif(self, fileName, add_sub, time):
+            # this function adds or subtract from the time of subtitle to set-up for videos.  
+            # fileName: the name of the subtitle file
+            # add_sub: option: type "add" for adding time or "sub" for subtract
+            # time: time in second the set-up difference
+            s.set_time_dif(subFile, arg3, float(arg4))
+        
+        else:
+            print "First parameter should be either 'convert' or 'set'"
+
+if __name__ == "__main__":
+   converter(sys.argv[1:])
